acc2cb964fd4b21bb253f41a8cc427bfc56420df
MikeDEV <mikierules109@gmail.com>
2022-08-29 11:20:41 -0400

    Add Unsandboxed Images
    Thanks @CST1229 for https://cst1229.github.io/files/extensions/unsandboxed-images.js
    
    Co-Authored-By: CST1229 <68464103+CST1229@users.noreply.github.com>

--- meower-utils.js
+++ meower-utils.js
@@ -7,6 +7,19 @@ class MeowerUtils {
 		this.window = window;
 		this.audio_player = new Audio();
 		this.popup_got_data = false;
+		if (!runtime.renderer._layerGroups.imagesExt) {
+			const layers = Object.keys(runtime.renderer._layerGroups);
+			layers.unshift("imagesExt");
+			runtime.renderer.setLayerGroupOrdering(layers);
+		}
+		
+		// To prevent destroying of images that weren't made with the extension
+		this.createdImages = new Set();
+		
+		// If we haven't already and we can, create the pen layer
+		if (runtime.ext_pen && !runtime.renderer._penSkinId) {
+			runtime.ext_pen._getPenLayerID();
+		}
     }
 
     getInfo () {
@@ -19,6 +32,55 @@ class MeowerUtils {
 			color2: '#b53e02',
 			color3: '#b53e02',
             "blocks": [
+				{
+					"opcode": 'getImage',
+					"blockType": "reporter",
+					"text": 'new image from URL [IMAGEURL]',
+					"arguments": {
+						"IMAGEURL": {
+							"type": "string",
+							"defaultValue": 'https://svelte.meower.org/assets/logo.22ae43ae.svg',
+						},
+					},
+				},
+				{
+					"opcode": 'drawImage',
+					"blockType": "command",
+					"text": 'stamp image [IMG] at x: [X] y: [Y] x scale: [XSCALE] y scale: [YSCALE]',
+					"arguments": {
+						"IMG": {
+							"type": "number",
+							"defaultValue": 0,
+						},
+						"X": {
+							"type": "number",
+							"defaultValue": 0,
+						},
+						"Y": {
+							"type": "number",
+							"defaultValue": 0,
+						},
+						"XSCALE": {
+							"type": "number",
+							"defaultValue": 100,
+						},
+						"YSCALE": {
+							"type": "number",
+							"defaultValue": 100,
+						},
+					},
+				},
+				{
+					"opcode": 'deleteImage',
+					"blockType": "command",
+					"text": 'delete image [IMG]',
+					"arguments": {
+						"IMG": {
+							"type": "number",
+							"defaultValue": 0,
+						},
+					},
+				},
 				{
                 	"opcode": 'playAudioFromURL',
                     "blockType": "command",
@@ -117,7 +179,78 @@ class MeowerUtils {
 			]
         };
     };
-
+	async getImage({IMAGEURL}) {
+		try {
+			const resp = await fetch(IMAGEURL);
+			const type = resp.headers.get("Content-Type");
+			
+			if (!resp.ok) {
+				return "";
+			}
+			
+			let skinId;
+			switch (type) {
+				case "image/svg+xml":
+				case "image/svg":
+					skinId = this.runtime.renderer.createSVGSkin(await resp.text());
+				break;
+				case "image/png":
+				case "image/bmp":
+                case "image/jpeg":
+					const image = new Image();
+					image.crossOrigin = "anonymous";
+					image.src = IMAGEURL;
+					await image.decode();
+					skinId = this.runtime.renderer.createBitmapSkin(image, 1);
+				break;
+				default:
+					return "";
+				break;
+			}
+			
+			const drawableId = this.runtime.renderer.createDrawable("imagesExt");
+			const img = this.runtime.renderer._allDrawables[drawableId];
+			img.updateVisible(false);
+			img.skin = vm.runtime.renderer._allSkins[skinId];
+			this.createdImages.add(drawableId);
+			return drawableId;
+		} catch(e) {
+			console.error("Error creating image:", e);
+			return "";
+		}
+	};
+	drawImage({IMG, X, Y, XSCALE, YSCALE}) {
+		try {
+			if (!this.runtime.renderer._penSkinId) return;
+			if (
+				!this.runtime.renderer._allDrawables[IMG] || !this.createdImages.has(IMG)
+			) return;
+			this.runtime.renderer._allDrawables[IMG].updatePosition([
+				Number(X) || 0,
+				Number(Y) || 0,
+			]);
+			this.runtime.renderer._allDrawables[IMG].updateScale([
+				XSCALE || 0,
+				YSCALE || 0,
+			]);
+			this.runtime.renderer.penStamp(
+				this.runtime.renderer._penSkinId, IMG
+			);
+		} catch(e) {
+			console.error("Error drawing image:", e);
+		}
+	};
+	deleteImage({IMG}) {
+		try {
+			if (
+				!this.runtime.renderer._allDrawables[IMG] || !this.createdImages.has(IMG)
+			) return;
+			this.runtime.renderer.destroyDrawable(IMG, "imagesExt");
+			this.createdImages.delete(IMG);
+		} catch(e) {
+			console.error("Error deleting image:", e);
+		}
+	};
 	playAudioFromURL({URL}) {
 		this.audio_player.pause();
 		this.audio_player.currentTime = 0;
@@ -213,11 +346,11 @@ class MeowerUtils {
 (function() {
     var extensionClass = MeowerUtils;
     if (typeof window === "undefined" || !window.vm) {
-		console.log("Sandboxed mode detected, Meower Utils cannot load in sandbox mode.");
+		console.log("Meower Utils cannot load in sandbox mode.");
     } else {
         var extensionInstance = new extensionClass(window.vm.extensionManager.runtime, window);
         var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance);
         window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName);
-		console.log("Unsandboxed mode detected. Good.");
+		console.log("Meower Utils loaded!");
     };
 })()
